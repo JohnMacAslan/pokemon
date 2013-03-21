8c61716df1d949c60a054eeae9fd617de64da7cb
John MacAslan <macaslje@rose-hulman.edu>
2013-03-21 16:28:16 -0400

    prettified domain class diagram, fixed the namespace problem

--- PokemonBejeweled/PokemonBejeweled2/Properties/AssemblyInfo.cs
+++ PokemonBejeweled/PokemonBejeweled2/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PokemonBejeweled2")]
+[assembly: AssemblyTitle("PokemonBejeweled")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Rose-Hulman Institute of Technology")]
-[assembly: AssemblyProduct("PokemonBejeweled2")]
+[assembly: AssemblyProduct("PokemonBejeweled")]
 [assembly: AssemblyCopyright("Copyright © Rose-Hulman Institute of Technology 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- PokemonBejeweled/PokemonBejeweled2/obj/Debug/MainWindow.g.i.cs
+++ PokemonBejeweled/PokemonBejeweled2/obj/Debug/MainWindow.g.i.cs
@@ -51,7 +51,7 @@ namespace PokemonBejeweled2 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PokemonBejeweled2;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PokemonBejeweled;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
