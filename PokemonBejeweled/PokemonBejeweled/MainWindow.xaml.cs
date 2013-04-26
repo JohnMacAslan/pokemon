01ceed9c2fd28d8dcc8f71354db67abde50d0d9d
John MacAslan <macaslje@rose-hulman.edu>
2013-04-26 04:51:22 -0400

    .

--- PokemonBejeweled/PokemonBejeweled/MainWindow.xaml.cs
+++ PokemonBejeweled/PokemonBejeweled/MainWindow.xaml.cs
@@ -21,10 +21,9 @@ namespace PokemonBejeweled
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<IBasicPokemonToken[,]> _history;
         private GameState gameState;
         private System.Windows.Controls.Primitives.UniformGrid gridBoard;
-        private Dictionary<Type, Brush> tokenColors = new Dictionary<Type,Brush>();
+        private Dictionary<Type, Brush> _tokenColors = tokenColors();
         private Boolean inMove;
         private int previousRow;
         private int previousColumn;
@@ -32,30 +31,9 @@ namespace PokemonBejeweled
         public MainWindow()
         {
             InitializeComponent();
-            tokenColors.Add(typeof(BulbasaurToken), Brushes.MediumSeaGreen);
-            tokenColors.Add(typeof(IvysaurToken), Brushes.MediumSeaGreen);
-            tokenColors.Add(typeof(VenusaurToken), Brushes.MediumSeaGreen);
-            tokenColors.Add(typeof(CharmanderToken), Brushes.Orange);
-            tokenColors.Add(typeof(CharmeleonToken), Brushes.Orange);
-            tokenColors.Add(typeof(CharizardToken), Brushes.Orange);
-            tokenColors.Add(typeof(SquirtleToken), Brushes.SkyBlue);
-            tokenColors.Add(typeof(WartortleToken), Brushes.SkyBlue);
-            tokenColors.Add(typeof(BlastoiseToken), Brushes.SkyBlue);
-            tokenColors.Add(typeof(PichuToken), Brushes.Yellow);
-            tokenColors.Add(typeof(PikachuToken), Brushes.Yellow);
-            tokenColors.Add(typeof(RaichuToken), Brushes.Yellow);
-            tokenColors.Add(typeof(TotodileToken), Brushes.CadetBlue);
-            tokenColors.Add(typeof(CroconawToken), Brushes.CadetBlue);
-            tokenColors.Add(typeof(FeraligatorToken), Brushes.CadetBlue);
-            tokenColors.Add(typeof(ChikoritaToken), Brushes.LightGreen);
-            tokenColors.Add(typeof(BayleefToken), Brushes.LightGreen);
-            tokenColors.Add(typeof(MeganiumToken), Brushes.LightGreen);
-            tokenColors.Add(typeof(CyndaquilToken), Brushes.OrangeRed);
-            tokenColors.Add(typeof(QuilavaToken), Brushes.OrangeRed);
-            tokenColors.Add(typeof(TyphlosionToken), Brushes.OrangeRed);
-            tokenColors.Add(typeof(DittoToken), Brushes.Pink);
             gameState = new GameState();
             gridBoard = this.GridBoard;
+            gameState.Grid.BoardDirtied += new BoardDirtiedEventHandler(delegate { updateGridBoard(); });
             inMove = false;
             previousColumn = 0;
             previousRow = 0;
@@ -109,27 +87,51 @@ namespace PokemonBejeweled
                 {
                     buttonEnumerator.MoveNext();
                     currentButton = (GridButton)buttonEnumerator.Current;
-
-                    currentButton.setBackgroundColor(tokenColors[gameState.Grid.Pokemon[r, c].GetType()]);
-                    currentButton.Background = (gameState.Grid.Pokemon[r, c].getPokemonPicture());
-           
-                    
-
-                    //if (null == gameState.Grid.Pokemon[r, c])
-                    //{
-                    //    currentButton.setBackgroundColor(Brushes.Black);
-                    //}
-                    //else
-                    //{
-                    //    currentButton.setBackgroundColor(tokenColors[gameState.Grid.Pokemon[r, c].GetType()]);
-                    //}
+                    if (null == gameState.Grid.Pokemon[r, c])
+                    {
+                        currentButton.setBackgroundColor(Brushes.Black);
+                    }
+                    else
+                    {
+                        currentButton.Background = (gameState.Grid.Pokemon[r, c].getPokemonPicture());
+                        //currentButton.setBackgroundColor(tokenColors[gameState.Grid.Pokemon[r, c].GetType()]);
+                    }
                 }
             }
             
             DependencyObject scope = FocusManager.GetFocusScope(this);
             FocusManager.SetFocusedElement(scope, this);
-            gridBoard.Dispatcher.Invoke(delegate() { }, System.Windows.Threading.DispatcherPriority.Render);
-            System.Threading.Thread.Sleep(500);
+            gridBoard.Dispatcher.Invoke(delegate()
+            {
+                System.Threading.Thread.Sleep(500);
+            }, System.Windows.Threading.DispatcherPriority.Render);
+        }
+
+        private static Dictionary<Type, Brush> tokenColors() {
+            Dictionary<Type, Brush> tokenColors = new Dictionary<Type, Brush>();
+            tokenColors.Add(typeof(BulbasaurToken), Brushes.MediumSeaGreen);
+            tokenColors.Add(typeof(IvysaurToken), Brushes.MediumSeaGreen);
+            tokenColors.Add(typeof(VenusaurToken), Brushes.MediumSeaGreen);
+            tokenColors.Add(typeof(CharmanderToken), Brushes.Orange);
+            tokenColors.Add(typeof(CharmeleonToken), Brushes.Orange);
+            tokenColors.Add(typeof(CharizardToken), Brushes.Orange);
+            tokenColors.Add(typeof(SquirtleToken), Brushes.SkyBlue);
+            tokenColors.Add(typeof(WartortleToken), Brushes.SkyBlue);
+            tokenColors.Add(typeof(BlastoiseToken), Brushes.SkyBlue);
+            tokenColors.Add(typeof(PichuToken), Brushes.Yellow);
+            tokenColors.Add(typeof(PikachuToken), Brushes.Yellow);
+            tokenColors.Add(typeof(RaichuToken), Brushes.Yellow);
+            tokenColors.Add(typeof(TotodileToken), Brushes.CadetBlue);
+            tokenColors.Add(typeof(CroconawToken), Brushes.CadetBlue);
+            tokenColors.Add(typeof(FeraligatorToken), Brushes.CadetBlue);
+            tokenColors.Add(typeof(ChikoritaToken), Brushes.LightGreen);
+            tokenColors.Add(typeof(BayleefToken), Brushes.LightGreen);
+            tokenColors.Add(typeof(MeganiumToken), Brushes.LightGreen);
+            tokenColors.Add(typeof(CyndaquilToken), Brushes.OrangeRed);
+            tokenColors.Add(typeof(QuilavaToken), Brushes.OrangeRed);
+            tokenColors.Add(typeof(TyphlosionToken), Brushes.OrangeRed);
+            tokenColors.Add(typeof(DittoToken), Brushes.Pink);
+            return tokenColors;
         }
        
     }
