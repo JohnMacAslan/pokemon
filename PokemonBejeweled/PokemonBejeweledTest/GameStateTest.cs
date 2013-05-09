0e15fba1beeb7832cc00acbcfbc9f974e8e2fae2
Rachel <agnerrl@rose-hulman.edu>
2013-05-09 18:10:34 -0400

    added some tests

--- PokemonBejeweled/PokemonBejeweledTest/GameStateTest.cs
+++ PokemonBejeweled/PokemonBejeweledTest/GameStateTest.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using PokemonBejeweled;
+using Rhino.Mocks;
 
 
 namespace PokemonBejeweledTest
@@ -12,66 +13,83 @@ namespace PokemonBejeweledTest
     [TestFixture()]
     public class GameStateTest
     {
-        private GameState game;
+        private GameState _gameState;
+        private MockRepository _mocks;
+        private PokemonBoard _mockBoard;
+
+        [SetUp]
+        public void setupMocks()
+        {
+            _mocks = new MockRepository();
+            _mockBoard = _mocks.PartialMock<PokemonBoard>();
+        }
+
+        [SetUp]
+        public void resetGameState()
+        {
+            _gameState = new GameState();
+        }
 
         [Test()]
         public void TestGameInitialization()
         {
-            game = new GameState();
-
-            Assert.AreEqual(0, game.Score);
-            Assert.AreEqual(120000, game.TimeLeft);
-            Assert.IsNotNull(game);
+            Assert.AreEqual(0, _gameState.Score);
+            Assert.AreEqual(120000, _gameState.TimeLeft);
+            Assert.IsNotNull(_gameState);
         }
 
         [Test()]
         public void TestGetScore()
         {
-            game = new GameState();
-            Assert.AreEqual(0, game.Score);
+            Assert.AreEqual(0, _gameState.Score);
         }
 
         [Test()]
         public void TestTimeLeftDefault()
         {
-            game = new GameState();
-            Assert.AreEqual(120000, game.TimeLeft);
+            Assert.AreEqual(120000, _gameState.TimeLeft);
         }
 
         [Test()]
         public void TestSetTimeLeft()
         {
-            game = new GameState();
-            game.TimeLeft = 2000;
-            Assert.AreEqual(2000, game.TimeLeft);
+            _gameState.TimeLeft = 2000;
+            Assert.AreEqual(2000, _gameState.TimeLeft);
         }
         [Test()]
         [ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void TestSetTimeWhenNegativeValue()
         {
-            game = new GameState();
-            game.TimeLeft = -100;
+            _gameState.TimeLeft = -100;
         }
 
         [Test()]
         public void TestSetTimeThatExceptionNotThrownWhenNoTimeLimit()
         {
-            game = new GameState();
-            game.TimeLeft = -1; // When -1, no time limit for game
-            Assert.AreEqual(-1, game.TimeLeft);
+            _gameState.TimeLeft = -1; // When -1, no time limit for game
+            Assert.AreEqual(-1, _gameState.TimeLeft);
         }
 
         [Test()]
         public void TestThatNewGameResetsGameValues()
         {
-            game = new GameState();
+            _gameState.TimeLeft = 1000;
+            Assert.AreEqual(1000, _gameState.TimeLeft);
 
-            game.TimeLeft = 1000;
-            Assert.AreEqual(1000, game.TimeLeft);
+            _gameState.newGame();
+            Assert.AreEqual(0, _gameState.Score);
+            Assert.AreEqual(120000, _gameState.TimeLeft);
+        }
 
-            game.newGame();
-            Assert.AreEqual(0, game.Score);
-            Assert.AreEqual(120000, game.TimeLeft);
+        [Test]
+        public void MakePlay_CanMakePlay_CallsBoardMakePlay()
+        {
+            _mockBoard.Expect(g => g.makePlay(0, 0, 0, 0));
+            _mockBoard.Replay();
+            _gameState.makePlay(0, 0);
+            _gameState.Board = _mockBoard;
+            _gameState.makePlay(0, 0);
+            _mockBoard.VerifyAllExpectations();
         }
     }
 }
